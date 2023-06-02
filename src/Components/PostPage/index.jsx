d77fb7b7127d5769fcca1162f5fb81e38699c619
svandu <svandu08@gmail.com>
2023-06-02 17:27:37 +0530

    post page component functionality done

--- src/Components/PostPage/index.jsx
+++ src/Components/PostPage/index.jsx
@@ -1,7 +1,30 @@
-function PostPage() {
+import './postpage.scss'
+import { useParams, Link } from 'react-router-dom';
+ 
+function PostPage({posts, handleDelete}) {
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() === id );
   return (
-    <main>
-        <h1>PostPage</h1>
+    <main className='postpage-container'>
+      <article className='post'>
+        {post && 
+            <>
+              <h2>{post.title}</h2>
+              <p className='postDate'>{post.datetime}</p>
+              <p className='postBody'>{post.body}</p>
+              <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+            </>
+        }
+        {!post && 
+            <>
+              <h2>Post Not Found</h2>
+              <p>Well, that's disappointing.</p>
+              <p>
+                <Link to="/">Visit Our HomePage</Link>
+              </p>
+            </>
+        }
+      </article>
     </main>
   )
 }
