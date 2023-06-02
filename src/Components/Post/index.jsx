45f49082ecfb2f943f499588a4fcbf2c464c8581
svandu <svandu08@gmail.com>
2023-06-02 17:28:00 +0530

    post component create

--- src/Components/Post/index.jsx
+++ src/Components/Post/index.jsx
@@ -1,19 +1,20 @@
-import { Link } from 'react-router-dom'
-import './post.scss'
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import "./post.scss";
 
 // eslint-disable-next-line react/prop-types
 function Post({ post }) {
   return (
-    <article className='post'>
-        <Link to={`/post/${post.id}`}>
-            <h2>{post.title}</h2>
-            <p className='postDate'>{post.datetime}</p>
-        </Link>
-        <p className='postBody'>{
-            (post.body).length <= 25 ? post.body : `${(post.body).slice(0,25)}...`
-        }</p>
+    <article className="post">
+      <Link to={`/post/${post.id}`}>
+        <h2>{post.title}</h2>
+        <p className="postDate">{post.datetime}</p>
+      </Link>
+      <p className="postBody">
+        {post.body.length <= 25 ? post.body : `${post.body.slice(0, 25)}...`}
+      </p>
     </article>
-  )
+  );
 }
 
-export default Post
+export default Post;
