f75f28156455915e54d2fbc19582e8e16fa4ce15
svandu <svandu08@gmail.com>
2023-06-02 18:43:13 +0530

    minor changes

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,6 +1,8 @@
+import './about.scss'
+
 function About() {
   return (
-    <main className="About">
+    <main className="about-container">
         <h2>About</h2>
         <p style={{marginTop:"1rem"}}>This blog app is a project in the where you can post your blogs related to the react post.</p>
     </main>
--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -21,7 +21,7 @@ function Nav({ search, setSearch }) {
       </form>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/post">Post</Link></li>
+        <li><Link to="/post">New Post</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
     </nav>
